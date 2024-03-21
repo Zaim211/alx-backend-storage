3cbcada619fb40b2e103a0b5ce20ec0755521aed
Zaim211 <yousseftitazaim@gmail.com>
2024-03-21 01:22:11 +0000

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,6 +2,6 @@
 
 SELECT band_name AS band_name, (IFNULL(split, '2022') - formed) AS lifespan
 	FROM metal_bands
-	WHERE band_name = 'Glam rock'
+	WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 	GROUP BY band_name
 	ORDER BY lifespan DESC;
