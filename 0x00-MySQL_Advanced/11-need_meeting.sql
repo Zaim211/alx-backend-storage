af23a988cebb31687172c4e3f1f4d4a9ac3f7cd9
Zaim211 <yousseftitazaim@gmail.com>
2024-03-21 23:26:49 +0000

    CREATE VIEW

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,4 +2,4 @@
 -- that have a score under 80 (strict) and no last_meeting or more than 1 month.
 
 CREATE VIEW need_meeting AS SELECT name FROM students WHERE score < 80
-AND (last_meeting IS NULL OR last_meeting < DATE(CURRENT() - 1 MONTH));
+AND (last_meeting IS NULL OR last_meeting < DATE(CURRENT() - INTERVAL 1 MONTH));
