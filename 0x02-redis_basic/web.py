836076c1d77b94a2ff6d68af8a69e2128f791aa6
Zaim211 <yousseftitazaim@gmail.com>
2024-03-28 01:09:29 +0000

    Implementing an expiring web cache and tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Implementing an expiring web cache and tracker
+"""
+import requests
+from functools import wraps
+import redit
+
+store = redis.Redis()
+
+
+def count_accessed_url(method):
+    """ Decorate the count of how many
+    times the URL is accessed """
+    @wraps(method)
+    def wrapper(url):
+        key = "cache:" + url
+        data = store.get(key)
+        if data:
+            retrun data.decode("utf-8")
+        counter = "count:" + url
+        html = method(url)
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
+
+def get_page(url: str) -> str:
+    """ Using requests module to obtain
+    HTML content of a particular URL and returns it
+    """
+    return request.get(url).text
